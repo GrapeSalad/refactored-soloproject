428e5fe8d2f3fa1dba1ba8199c0d1a08e4812019
David Wilson <davidtheadmiral@gmail.com>
2017-04-28 10:18:06 -0700

    create ui branching logic, slight error testing, create business logic math to compute quiz values

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,33 @@ var quizTotal = function(q1Answer, q2Answer, q3Answer, q4Answer, q5Answer, q6Ans
   return (q1Answer + q2Answer + q3Answer + q4Answer + q5Answer + q6Answer + q7Answer + q8Answer);
 };
 
+// var chosenPath = function() {
+//   if (quizTotal >= 8 || quizTotal <= 40){
+//     console.log("first if working");
+//     if (quizTotal >= 8 && quizTotal <= 14) {
+//       var pathValue = 1;
+//       return pathValue;
+//     }
+//     else if (quizTotal >= 15 && quizTotal <= 21) {
+//       var pathValue = 2;
+//       return pathValue;
+//     }
+//     else if (quizTotal >= 22 && quizTotal <= 28) {
+//       var pathValue = 3;
+//       return pathValue;
+//     }
+//     else if (quizTotal >= 29 && quizTotal <= 34) {
+//       var pathValue = 4;
+//       return pathValue;
+//     }
+//     else {
+//       var pathValue = 5;
+//       return pathValue;
+//       console.log("Stuck in branching");
+//     }
+//   }
+// };
+
 //UI logic
 $(document).ready(function() {
   $(document).submit(function(event) {
@@ -22,8 +49,28 @@ $(document).ready(function() {
 
     var result = quizTotal(q1Answer, q2Answer, q3Answer, q4Answer, q5Answer, q6Answer, q7Answer, q8Answer);
 
+    var chosenPath = result;
+
+    if (chosenPath >= 8 && chosenPath <= 14) {
+      console.log("chosenPath first if ", chosenPath);
+    }
+    else if (chosenPath >= 15 && chosenPath <= 21){
+      console.log("chosenPath first branch ", chosenPath);
+    }
+    else if (chosenPath >= 22 && chosenPath <= 28){
+      console.log("chosenPath second branch ", chosenPath);
+    }
+    else if (chosenPath >= 29 && chosenPath <= 34){
+      console.log("chosenPath third branch ", chosenPath);
+    }
+    else {
+      console.log("else end ", chosenPath);
+    }
+
+
 //OUTPUT
-    $("#output").text("Well, " + firstName + " " + lastName + ", it looks like you have to pursue:");
+    $("#output").text("Well, " + firstName + " " + lastName + ", it looks like you have to pursue: " + chosenPath);
+
 //ERROR CHECKING
     // console.log(firstName + " " + lastName, typeof(firstName));
     // console.log(q1Answer, typeof(q1Answer));
@@ -34,6 +81,6 @@ $(document).ready(function() {
     // console.log(q6Answer, typeof(q6Answer));
     // console.log(q7Answer, typeof(q7Answer));
     // console.log(q8Answer, typeof(q8Answer));
-    console.log(result, typeof(result));
+    console.log("Total points from quiz: ", result, typeof(result));
   });
 });
